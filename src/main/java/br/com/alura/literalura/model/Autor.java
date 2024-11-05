f63c21148f7eddda8202677aa5e422755df642b5
Luiz Ferreira <luhaferr@gmail.com>
2024-11-05 14:40:34 -0300

    pacote model, suas clases e mapeamento

---
+++ src/main/java/br/com/alura/literalura/model/Autor.java
@@ -0,0 +1,50 @@
+package br.com.alura.literalura.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "autores")
+public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true)
+    private String nome;
+    private int anoNascimento;
+    private int anoFalecimento;
+    private List<Livro> livros;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoNascimento() {
+        return anoNascimento;
+    }
+
+    public void setAnoNascimento(int anoNascimento) {
+        this.anoNascimento = anoNascimento;
+    }
+
+    public int getAnoFalecimento() {
+        return anoFalecimento;
+    }
+
+    public void setAnoFalecimento(int anoFalecimento) {
+        this.anoFalecimento = anoFalecimento;
+    }
+}
---
+++ src/main/java/br/com/alura/literalura/model/DadosLivro.java
@@ -0,0 +1,10 @@
+package br.com.alura.literalura.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosLivro(@JsonAlias("title") String titulo,
+                         @JsonAlias("languages") String idioma,
+                         @JsonAlias("download_count") Long numeroDownloads) {
+}
